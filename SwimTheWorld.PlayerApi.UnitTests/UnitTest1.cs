14b6eb5db7d964f383b6c0dd188b3f3797c5cc26
LeannaDixon <leannapdixon@outlook.com>
2025-10-06 07:35:50 +0100

    Initial commit with basic structure for Player.API

---
+++ SwimTheWorld.PlayerApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SwimTheWorld.PlayerApi.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
