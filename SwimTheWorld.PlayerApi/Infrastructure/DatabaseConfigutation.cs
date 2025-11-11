f76933cf1cbb0e91aed40ff4a5d1f03b32039af2
LeannaDixon <leannapdixon@outlook.com>
2025-11-11 21:31:34 +0000

    WIP: Added dockercompose from Infra project to here fopr speed. Need to configure it properly.
    Set up local connection to Postgress sb.
    Added .ApplyMigration() to apply migrations automatically.
    Moved gitignore to outside the repo (opps).

---
+++ SwimTheWorld.PlayerApi/Infrastructure/DatabaseConfigutation.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SwimTheWorld.PlayerApi.Infrastructure;
+
+public static class DatabaseConfigutation
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PlayerDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
